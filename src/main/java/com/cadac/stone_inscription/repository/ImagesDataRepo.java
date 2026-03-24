c8505c05b8ed4b3218445eeda3fdc8b4bae5b57b
Bavithbabu <bavithbabu25@gmail.com>
2026-03-24 17:17:34 +0530

    Fixed the image duplication

--- src/main/java/com/cadac/stone_inscription/repository/ImagesDataRepo.java
+++ src/main/java/com/cadac/stone_inscription/repository/ImagesDataRepo.java
@@ -1,17 +1,18 @@
 package com.cadac.stone_inscription.repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cadac.stone_inscription.entity.ImagesData;
 
-import dev.brachtendorf.jimagehash.hash.Hash;
-
 @Repository
 public interface ImagesDataRepo extends MongoRepository<ImagesData, String> {
 
-          Optional<ImagesData> findFirstByMetadata_ImageHashValue(String imageHashValue);
-   
+    Optional<ImagesData> findFirstByMetadata_ImageHashValueAndPostIdIn(String imageHashValue,
+            Collection<ObjectId> postIds);
+
 }
